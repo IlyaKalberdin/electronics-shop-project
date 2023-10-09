62af29543538d5afd4e37efaeabf6cff6fa7641c
Ilya Kalberdin <ilyakalberdin@gmail.com>
2023-10-09 03:10:33 +0300

    Добавлены новые тесты для дз2

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,4 @@
 import pytest
-
 from src.item import Item
 
 
@@ -17,6 +16,14 @@ def test_Item_attr(test_Item):
     assert test_Item.quantity == 25
 
 
+def test_Item_name_getter(test_Item):
+    test_Item.name = "Смартфон"
+    assert test_Item.name == "Смартфон"
+
+    test_Item.name = "СуперСмартфон"
+    assert test_Item.name == "Смартфон"
+
+
 def test_Item_calculate_total_price(test_Item):
     """Тест метода calculate_total_price класса Item"""
     assert test_Item.calculate_total_price() == 1237500.00
@@ -31,4 +38,17 @@ def test_Item_apply_discount(test_Item):
 
 def test_Item_all(test_Item):
     """Тест общего атрибута all класса Item"""
-    assert len(Item.all) == 4
+    assert len(Item.all) == 5
+
+
+def test_Item_instantiate_from_csv():
+    """Тест для метода instantiate_from_csv класса Item"""
+    Item.instantiate_from_csv("src/items.csv")
+    assert len(Item.all) == 5
+
+
+def test_Item_string_to_number():
+    """Тест для метода string_to_number класса Item"""
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
