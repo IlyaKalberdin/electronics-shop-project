782ca99eb7e9dc022a69b06ffc47294cff19c840
Ilya Kalberdin <ilyakalberdin@gmail.com>
2023-10-09 03:11:07 +0300

    В класс добавлены новые методы для дз2

--- src/item.py
+++ src/item.py
@@ -1,10 +1,12 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 0.85
     all = []
-
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -14,11 +16,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
-
 
     def calculate_total_price(self) -> float:
         """
@@ -28,9 +29,33 @@ class Item:
         """
         return self.price * self.quantity
 
-
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * Item.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            print("Exception: Длина наименования товара превышает 10 символов")
+        else:
+            self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        cls.all = []
+
+        with open(path, encoding="cp1251") as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            for row in reader:
+                item = cls(row["name"], row["price"], row["quantity"])
+
+    @staticmethod
+    def string_to_number(string):
+        return int(float(string))
